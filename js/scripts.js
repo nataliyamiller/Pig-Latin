0ba7a8b0b3116493ba5c1c7cbd91384a67d34f69
Nataliya Miller <nataliyamiller82@gmail.com>
2015-09-15 11:02:19 -0700

    Refactored function to a vowel checker function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
+var finalWord;
 
 
-var pigLatin = function(userWord) {
+var vowelChecker = function(userWord) {
   var vowels = ["a", "e", "i", "o", "u"];
+  var startsWithVowel = false;
   for (var i = 0; i < vowels.length; i++) {
     if (userWord[0] === vowels[i]) {
-      userWord += "ay";
+      startsWithVowel = true;
     }
   }
-  return userWord;
+  return startsWithVowel;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,17 @@
 describe('pigLatin', function() {
-  it("is adding ay to the end if word starts with a vowel", function() {
-    expect(pigLatin("orange")).to.equal("orangeay");
+
+  it("is returning true if word starts with a vowel", function() {
+    expect(vowelChecker("orange")).to.equal(true);
   });
 
-  it("is returning unchanged word if word doesn't start with a vowel", function() {
-    expect(pigLatin("bobcat")).to.equal("bobcat");
+  it("is returning false if word doesn't start with a vowel", function() {
+    expect(vowelChecker("bob")).to.equal(false);
   });
+  // it("is moving first letter to the end of word if word doesn't start with a vowel", function() {
+  //   expect(pigLatin("bobcat")).to.equal("obcatbay");
+  // });
+  //
+  // it("is adding ay to the end if word starts with a vowel", function() {
+  //   expect(pigLatin("orange")).to.equal("orangeay");
+  // });
 });
