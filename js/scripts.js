3ef83c4052bb7d2fffc3e1492c77b6e9091b4ffa
Nataliya Miller <nataliyamiller82@gmail.com>
2015-09-15 16:33:26 -0700

    Adds pigLatin method and passes the specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 var finalWord;
 var vowels = ["a", "e", "i", "o", "u"];
+var ay = "ay";
 
 var vowelChecker = function(userWord) {
   return vowels.indexOf(userWord[0]) > -1;
 }
 
-var consonantChecker = function(userWord) {
+var yChecker = function(userWord) {
+  return userWord[0] === "y";
+}
 
+var vowelMover = function(userWord) {
+  return userWord + ay;
+}
+
+var consonantIndexFinder = function(userWord) {
   if(!vowelChecker(userWord)) {
     for (var i = 0; i < userWord.length; i++) {
       if(vowels.indexOf(userWord[i]) > -1) {
@@ -17,21 +25,35 @@ var consonantChecker = function(userWord) {
 }
 
 var consonantMover = function(userWord) {
-  if (!vowelChecker(userWord)) {
-    var index = consonantChecker(userWord);
-    finalWord = userWord.slice(index) + userWord.slice(0, index);
-  } else {
-    var finalWord = userWord;
+  var index = consonantIndexFinder(userWord);
+  if (index >= 0) {
+    return userWord.slice(index) + userWord.slice(0, index) + ay;
   }
-  return finalWord += "ay";
 }
 
 var quChecker = function(userWord) {
-  var index = consonantChecker(userWord);
+  var index = consonantIndexFinder(userWord);
   var newWord = userWord.slice(0, index + 1);
     if (newWord.indexOf("qu") > -1) {
       return true;
     } else {
       return false;
     }
+}
+
+var quMover = function(userWord) {
+  if(quChecker(userWord)) {
+    var index = consonantIndexFinder(userWord);
+    return userWord.slice(index + 1) + userWord.slice(0, index + 1) + ay;
+  }
+}
+
+var pigLatin = function(userWord) {
+  if(vowelChecker(userWord) || yChecker(userWord)) {
+    return vowelMover(userWord);
+  } else if (quChecker(userWord)) {
+    return quMover(userWord);
+  } else {
+    return consonantMover(userWord);
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -8,23 +8,31 @@ describe('vowelChecker', function() {
   });
 });
 
-describe('consonantChecker', function() {
+describe('yChecker', function() {
+  it("is returning true if word starts with a letter y", function() {
+    expect(yChecker("yrra")).to.equal(true);
+  });
+});
+
+describe('vowelMover', function() {
+  it("is adding ay to the end of the word if the word starts with a vowel", function() {
+    expect(vowelMover("orange")).to.equal("orangeay");
+  });
+});
+
+describe('consonantIndexFinder', function() {
   it("is returning index of a first vowel in a word", function() {
-    expect(consonantChecker("brake")).to.equal(2);
+    expect(consonantIndexFinder("brake")).to.equal(2);
   });
 
   it("is returning index of a first vowel in a word", function() {
-    expect(consonantChecker("mother")).to.equal(1);
+    expect(consonantIndexFinder("mother")).to.equal(1);
   });
 });
 
 describe('consonantMover', function() {
   it("is moving first letter to the end of word if word doesn't start with a vowel", function() {
     expect(consonantMover("mother")).to.equal("othermay");
-  });
-
-  it("is adding ay to the end if word starts with a vowel", function() {
-    expect(consonantMover("orange")).to.equal("orangeay");
   });
 
   it("is moving two first letters to the end of word if word starts with two consonants", function() {
@@ -39,5 +47,42 @@ describe('consonantMover', function() {
 describe('quChecker', function() {
   it("is recognizing the qu letters at the beginning of the word if the word starts with a consonant", function() {
     expect(quChecker("squshel")).to.equal(true);
+  });
+
+  it("is recognizing the qu letters at the beginning of the word if the word starts with a qu", function() {
+      expect(quChecker("quater")).to.equal(true);
+  });
+
+});
+
+describe('quMover', function() {
+  it("is recognizing the qu substring at the beginning of word and moves qu to the end", function() {
+    expect(quMover("squshel")).to.equal("shelsquay");
+  });
+
+  it("is recognizing the qu substring at the start of word and moves qu to the end", function() {
+    expect(quMover("quarter")).to.equal("arterquay");
+  });
+});
+
+describe('pigLatin', function() {
+  it("is adding ay to the end of the word if the word starts with a vowel", function() {
+    expect(pigLatin("orange")).to.equal("orangeay");
+  });
+
+  it("is moving first letter to the end of word if word doesn't start with a vowel", function() {
+    expect(pigLatin("mother")).to.equal("othermay");
+  });
+
+  it("is recognizing the qu substring at the beginning of word and moves qu to the end", function() {
+    expect(pigLatin("squshel")).to.equal("shelsquay");
+  });
+
+  it("is recognizing the qu substring at the start of word and moves qu to the end", function() {
+    expect(pigLatin("quarter")).to.equal("arterquay");
+  });
+
+  it("is not moving y to the end of the word if the word starts with y", function() {
+    expect(pigLatin("yaaay")).to.equal("yaaayay");
   });
 });
