a9c4d96db9a034c8beb4edd5b122509ae8849022
Nataliya Miller <nataliyamiller82@gmail.com>
2015-09-15 11:15:49 -0700

    passes specs for moving the first letter to the end of word if word starts with a consonant

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,23 @@ var finalWord;
 var vowelChecker = function(userWord) {
   var vowels = ["a", "e", "i", "o", "u"];
   var startsWithVowel = false;
+
   for (var i = 0; i < vowels.length; i++) {
     if (userWord[0] === vowels[i]) {
       startsWithVowel = true;
     }
   }
   return startsWithVowel;
+}
+
+
+
+var consonantModifier = function(userWord) {
+
+  if (!vowelChecker(userWord)) {
+    finalWord = userWord.slice(1) + userWord.slice(0, 1);
+  } else {
+    finalWord = userWord;
+  }
+  return finalWord += "ay";
 }
--- spec/specs.js
+++ spec/specs.js
@@ -7,11 +7,12 @@ describe('pigLatin', function() {
   it("is returning false if word doesn't start with a vowel", function() {
     expect(vowelChecker("bob")).to.equal(false);
   });
-  // it("is moving first letter to the end of word if word doesn't start with a vowel", function() {
-  //   expect(pigLatin("bobcat")).to.equal("obcatbay");
-  // });
-  //
-  // it("is adding ay to the end if word starts with a vowel", function() {
-  //   expect(pigLatin("orange")).to.equal("orangeay");
-  // });
+
+  it("is moving first letter to the end of word if word doesn't start with a vowel", function() {
+    expect(consonantModifier("cobcat")).to.equal("obcatcay");
+  });
+
+  it("is adding ay to the end if word starts with a vowel", function() {
+    expect(consonantModifier("orange")).to.equal("orangeay");
+  });
 });
