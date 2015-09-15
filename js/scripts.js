34eeb27bbaf7d1631d3415137753332812036069
Nataliya Miller <nataliyamiller82@gmail.com>
2015-09-15 10:29:42 -0700

    Passes test for checking if the word starts with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+
+
 var pigLatin = function(userWord) {
-  return userWord + "ay";
-}; 
+  var vowels = ["a", "e", "i", "o", "u"];
+  for (var i = 0; i < vowels.length; i++) {
+    if (userWord[0] === vowels[i]) {
+      userWord += "ay";
+    }
+  }
+  return userWord;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('pigLatin', function() {
   it("is adding ay to the end if word starts with a vowel", function() {
-    expect(pigLatin("orange")).to.equal("orangeay")
+    expect(pigLatin("orange")).to.equal("orangeay");
+  });
+
+  it("is returning unchanged word if word doesn't start with a vowel", function() {
+    expect(pigLatin("bobcat")).to.equal("bobcat");
   });
 });
